e7d643805f786a47630a30686bf69a93c8a3acfc
JaimeOrtega1 <jortegat2@unemi.edu.ec>
2023-08-04 14:32:12 -0500

    Formulario de clientes y cuadros listo y funcionales con imagenes

--- django-backend/store/forms.py
+++ django-backend/store/forms.py
@@ -1,23 +1,25 @@
 from django import forms
-from .models import Cuadro
+from .models import Cuadro , Categoria
 from sale.models import Cliente
 
 class CuadroForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['categoria'].widget = forms.Select(attrs={'class': 'form-control form-control-sm'})
+        self.fields['categoria'].queryset = Categoria.objects.all()
+
     class Meta:
         model = Cuadro
-        fields = ['codigo', 'categoria','titulo','descripcion','autor','año','imagen']
+        fields = ['codigo', 'categoria', 'titulo', 'descripcion', 'autor', 'año', 'imagen']
         widgets = {
             'codigo': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
-            'categoria': forms.SelectMultiple(attrs={'class': 'form-control form-control-sm'}),
             'titulo': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
             'descripcion': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
             'autor': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
             'año': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
-            'imagen': forms.FileInput(attrs={'class': 'form-control-file'}),
-
-
-
+            'imagen': forms.FileInput(attrs={'class': 'form-control form-control-sm'}),
         }
+        
 
 class ClienteForm(forms.ModelForm):
     class Meta:
